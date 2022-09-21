e7388f6db08ef7771fcee042d437e2832ed289fc
Gary Jacobs <gjacobs@trafficcast.com>
2022-09-21 14:56:55 -0400

    Build fix...local build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 
 android {
-    compileSdk = 32
+    compileSdk = 33
 
     defaultConfig {
         applicationId = "com.gmjproductions.simplemap"
         minSdk = 26
-        targetSdk = 32
+        targetSdk = 33
         versionCode = 1
         versionName = "1.0"
 
@@ -40,18 +40,19 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = rootProject.extra["compose_version"] as String
+        kotlinCompilerExtensionVersion = "1.3.1"
         //kotlinCompilerVersion = "1.4.32"
     }
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0-RC")
-    implementation("androidx.core:core-ktx:1.8.0")
-    implementation("androidx.appcompat:appcompat:1.4.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.1")
+    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.appcompat:appcompat:1.5.1")
     implementation("com.google.android.material:material:1.6.1")
-    implementation ("androidx.constraintlayout:constraintlayout-compose:1.1.0-alpha02")
-    implementation("androidx.activity:activity-compose:1.4.0")
+    implementation ("androidx.constraintlayout:constraintlayout-compose:1.1.0-alpha03")
+    implementation("androidx.activity:activity-compose:1.5.1")
+
     implementation("androidx.compose.ui:ui:${rootProject.extra["compose_version"]}")
     implementation("androidx.compose.material:material:${rootProject.extra["compose_version"]}")
     implementation("androidx.compose.ui:ui-tooling:${rootProject.extra["compose_version"]}")
@@ -60,16 +61,16 @@ dependencies {
     implementation("org.osmdroid:osmdroid-android:6.1.10")
     implementation("com.github.MKergall:osmbonuspack:6.7.0")
     implementation("com.localebro:okhttpprofiler:1.0.8")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.4.2")
-    implementation("androidx.navigation:navigation-ui-ktx:2.4.2")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.5.2")
+    implementation("androidx.navigation:navigation-ui-ktx:2.5.2")
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
-    implementation ("com.google.android.gms:play-services-location:19.0.1")
+    implementation ("com.google.android.gms:play-services-location:20.0.0")
     implementation ("androidx.datastore:datastore-preferences-core:1.0.0")
     implementation ("androidx.datastore:datastore-preferences:1.0.0")
     // Local module
-    //implementation(project(mapOf("path" to ":openchargemap")))
+    implementation(project(mapOf("path" to ":openchargemap")))
     // Public repo library
-    implementation("com.github.dinzdale:openchargemap_android_library:-SNAPSHOT")
+    //implementation("com.github.dinzdale:openchargemap_android_library:-SNAPSHOT")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.3")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,17 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
-    val compose_version by extra("1.0.0")
-    val kotlin_version by extra("1.6.21")
+    val compose_version by extra("1.2.1")
+    val kotlin_version by extra("1.7.1")
 
     repositories {
         google()
         mavenCentral()
+        maven (url= "https://plugins.gradle.org/m2/")
+
     }
     dependencies {
         classpath("com.android.tools.build:gradle:7.0.4")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.10")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.10")
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle.kts files
