04b42997c251165cf8923dc12b43de69490ce4b2
Gary Jacobs <gjacobs@trafficcast.com>
2022-10-02 14:38:51 -0400

    Update for compose compatibility. Java 8, plugin, etc.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,6 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.gmjproductions.simplemap"
+        buildToolsVersion = "33.0.0"
         minSdk = 26
         targetSdk = 33
         versionCode = 1
@@ -26,22 +27,22 @@ android {
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
-                         )
+            )
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
     }
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+        jvmTarget = "1.8"
     }
     buildFeatures {
         compose = true
     }
+
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.3.1"
-        //kotlinCompilerVersion = "1.4.32"
+        kotlinCompilerExtensionVersion = "1.3.0"
     }
 }
 
@@ -50,23 +51,24 @@ dependencies {
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.5.1")
     implementation("com.google.android.material:material:1.6.1")
-    implementation ("androidx.constraintlayout:constraintlayout-compose:1.1.0-alpha03")
-    implementation("androidx.activity:activity-compose:1.5.1")
+    implementation("androidx.constraintlayout:constraintlayout-compose:1.1.0-alpha04")
+    implementation("androidx.activity:activity-compose:1.6.0")
 
     implementation("androidx.compose.ui:ui:${rootProject.extra["compose_version"]}")
     implementation("androidx.compose.material:material:${rootProject.extra["compose_version"]}")
     implementation("androidx.compose.ui:ui-tooling:${rootProject.extra["compose_version"]}")
     implementation("androidx.compose.runtime:runtime:${rootProject.extra["compose_version"]}")
-    implementation("androidx.compose.runtime:runtime-livedata:${rootProject.extra["compose_version"]}")
+    implementation(
+        "androidx.compose.runtime:runtime-livedata:${rootProject.extra["compose_version"]}")
     implementation("org.osmdroid:osmdroid-android:6.1.10")
     implementation("com.github.MKergall:osmbonuspack:6.7.0")
     implementation("com.localebro:okhttpprofiler:1.0.8")
     implementation("androidx.navigation:navigation-fragment-ktx:2.5.2")
     implementation("androidx.navigation:navigation-ui-ktx:2.5.2")
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
-    implementation ("com.google.android.gms:play-services-location:20.0.0")
-    implementation ("androidx.datastore:datastore-preferences-core:1.0.0")
-    implementation ("androidx.datastore:datastore-preferences:1.0.0")
+    implementation("com.google.android.gms:play-services-location:20.0.0")
+    implementation("androidx.datastore:datastore-preferences-core:1.0.0")
+    implementation("androidx.datastore:datastore-preferences:1.0.0")
     // Local module
     implementation(project(mapOf("path" to ":openchargemap")))
     // Public repo library
@@ -74,5 +76,6 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.3")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:${rootProject.extra["compose_version"]}")
+    androidTestImplementation(
+        "androidx.compose.ui:ui-test-junit4:${rootProject.extra["compose_version"]}")
 }
